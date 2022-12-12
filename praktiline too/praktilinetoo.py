ce2f75e7d1ef7a4d8fdcfad369b077ac95289abb
Макс Мискевич <miskevich228@gmail.com>
2022-12-12 13:35:08 +0200

    Добавьте файлы проекта.

---
+++ praktiline too/praktilinetoo.py
@@ -0,0 +1,74 @@
+﻿from math import *
+from random import *
+#1.
+#print("Puu läbimõõdu arvutamine")
+##Kirjuta programm, mis küsib kasutaja käest puu ümbermõõdu ning teatab selle peale puu läbimõõdu.
+#C=float(input("dlina okruzhnosti: "))
+#d=round(C/pi,2)
+#print(f"puu labimoot = {d}")
+
+#2 Рассчитайте длину диагонали прямоугольного участка земли размерами Nm x Mm в командной строке Python. N и M спрашивают пользователя.
+#from math import *
+#import math
+#print("ristkuliku omadused")
+#M=float(input("ristkuliku esimene kulg"))
+#N=float(input("ristkuliku esimene kulg"))
+#C=math.sqrt(N**2+M**2)
+#print("ristkuliku pikkus on", round(C,2))
+#print()
+ 
+#Aeg = float(input("mitutundi kulus soiduks"))
+#teepikkus = float(input("Mitu kilomeetrit soitsid? "))
+#kiirus = teepikkus / Aeg
+#print("Siny kiirus oli " + str(kiirus) + " km/h")
+##4
+#print("aritmeetiline keskmine")
+#A1=int(input("esimene arv"))
+#A2=int(input("Teine arv"))
+#A3=int(input("kolmas arv"))
+#A4=int(input("neljas arv"))
+#A5=int(input("viies arv"))
+#K=(A1+A1+A3+A4+A5)/5
+#print(f"keskmine on {K}")
+#5
+#print("  @..@ ")
+#print(" (----)")
+#print(" (\__/)")
+#print('^ ^ "" ^ ^')
+#6
+
+
+#from random import *
+#print("Arvutamine kolmnurga umbermoodu")
+#a=randint(0,100)
+#b=randint(0,100)
+#c=randint(0,100)
+#print(f"a={a}\nb={b}\nc={c}")
+#P=a+b+c 
+#print("perimetr on {P}")
+#7
+
+#print("pitsa jootraha arvutamine")
+#P=randint(1,6)
+#summa=(12.9*1.1)/P 
+#print(f"{P}-st, igauks maksab {summa}")
+
+#8
+#from random import *
+#l=float(input("sisestage taidetud kutuse liitrid: "))
+#km=float(input("sisestage labitud kilomeetrid: "))
+#kulu:(l/km)*100
+#print(f"kutusekulu",kulu)
+ #9
+ #print("rulluisutajad")
+ #M=int(input("minutid:"))
+ #M=M/60
+ #tee=M*29.9
+ #print(f"jouab {tee} km")
+
+ #10
+ print("Ajateisendus")
+ M=int(input("sisesta aja minutites")) #1h=60m
+ H=M//60
+ M=M%60
+ print(f"{H}:{M}")
